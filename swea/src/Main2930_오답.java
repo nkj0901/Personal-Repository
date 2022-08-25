3a54f18a2ec1115f36f6a655c59fd0c886175908
NAM KIJUNG <nkj0901@naver.com>
2022-08-25 09:17:43 +0900

    0825 알고리즘 풀이

--- swea/src/Main2930_오답.java
+++ swea/src/Main2930_오답.java
@@ -1,11 +1,9 @@
-package swea;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main2930 {
+public class Main2930_오답 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
