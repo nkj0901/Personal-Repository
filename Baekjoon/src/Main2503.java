2b588d8b93c97e5a292b14a8305198aa9231498c
pingu0130 <penguin1998@daum.net>
2022-09-16 17:57:42 +0900

    0916

--- Baekjoon/src/Main2503.java
+++ Baekjoon/src/Main2503.java
@@ -26,33 +26,52 @@ public class Main2503 {
 			ans[i][1] = Integer.parseInt(st.nextToken());
 		}
 		int num = 123; // 세자리 숫자중에 제일 작은 수
-		int cnt = 0;
-		while (num == 987) {
-			boolean check = true;
+		int A =0;
+		//숫자 하나씩 늘려가면서 볼거야
+		while (num <=987) {
+			int cnt = 0;
 			int strike = 0;
 			int ball = 0;
-			String str = num + "";
-
+			String str = num + ""; //숫자 문자열로 만들어줘~
+			
+			//이 숫자가 주어진 숫자들과 비교했을 때, 스타라이크와 볼이 몇개인지 확인
+			//주어진 답과 수가 일치하면 정답 후보가 됨(cnt++)
 			for (int i = 0; i < 4; i++) {
-				for (int j = 0; j < 3; j++) {
-					if (arr[i][j] == str.charAt(0)) {
+				
+					if (arr[i][0] == str.charAt(0)) {
 						strike++;
-					} else if (arr[i][j] == str.charAt(1)) {
+					} else if (arr[i][0] == str.charAt(1)) {
 						ball++;
-					} else if (arr[i][j] == str.charAt(2)) {
+					} else if (arr[i][0] == str.charAt(2)) {
+						ball++;
+					}	
+					
+					if (arr[i][1] == str.charAt(0)) {
+						ball++;
+					} else if (arr[i][1] == str.charAt(1)) {
+						strike++;
+					} else if (arr[i][1] == str.charAt(2)) {
 						ball++;
 					}
-				}
-
-				if (ans[i][0] != strike || ans[i][1] != ball) {
-					check = false;
-				} else
-					continue;
+					
+					if (arr[i][2] == str.charAt(0)) {
+						ball++;
+					} else if (arr[i][2] == str.charAt(1)) {
+						ball++;
+					} else if (arr[i][2] == str.charAt(2)) {
+						strike++;
+					}
+					if (ans[i][0] == strike || ans[i][1] == ball) {
+						cnt++;
+					}
 			}
-			if (check == true) cnt++;
+			if (cnt == 4) {
+				A++;
+			}
+			System.out.println(num);
 			num++;
 		}
-		bw.write(cnt+"");
+		bw.write(A+"");
 		bw.flush();
 		br.close();
 		bw.close();
