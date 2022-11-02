264ec343ffa5186e90845896ff9a614caabf65b8
nkj0901 <nkj0901@naver.com>
2022-11-02 17:56:08 +0900

    1102

---
+++ SSAFY/java_workspace/vue/vue2/3_module/module1.js
@@ -0,0 +1,11 @@
+const title = '꼐산기 모듈'
+
+function add(i,j){
+    return i+j;
+}
+
+function sub(i, j){
+    return i-j;
+}
+
+export{title, add, sub}
---
+++ SSAFY/java_workspace/vue/vue2/3_module/test2.js
@@ -0,0 +1,29 @@
+import cal from './module2.js';
+
+const app = new Vue({
+    el:'#app',
+    data() {
+        return{
+            num1:0,
+            num2:0,
+            op: '-',
+            result: 0
+        }
+    },
+    methods: {
+        doCal() {
+            if(this.op === '+')
+            this.result = cal.add(this.num1, this.num2)
+            else
+            this.result = cal.sub(this.num1, this.num2)
+        }
+    },
+    watch: {
+        op(val) {
+            if(val === '+')
+            this.result = cal.addadd(this.num1, this.num2)
+            else
+            this.result = cal.sub(this.num1, this.num2)
+        }
+    }
+})
